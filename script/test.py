8c50ab5a1167adc4a3be1ef7337f46ab3cb1e471
PC-20180212VXOZ\Administrator <yangtze10@qq.com>
2018-03-30 20:36:31 +0800

    初始化

---
+++ script/test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+class Test():
+    def setup(self):
+        pass
+    def teardown(self):
+        pass
+    def test_a(self):
+        assert 1
+
+    def test_b(self):
+        assert 1
